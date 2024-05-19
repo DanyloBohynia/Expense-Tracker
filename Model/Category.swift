954c98d88ce558ee90f5b77e461bb625103506da
DanyloBohynia <danilabogynia@gmail.com>
2024-05-18 22:35:33 -0400

    Added date handling and dynamic visual effects to Recents view, and extended View with custom spacing and formatting methods

---
+++ Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Expense Tracker
+//
+//  Created by danqya on 12/05/2024.
+//
+
+import SwiftUI
+
+enum Category: String {
+    case income = "Income"
+    case expense = "Expense"
+    
+}
--- View/TabViews/Recents.swift
+++ View/TabViews/Recents.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct Recents: View {
     // User Properties
     @AppStorage ("userName") private var userName: String = ""
+    // View Properties
+    @State private var startDate: Date = .now.startOfMonth
+    @State private var endDate: Date = .now.endOfMonth
     var body: some View {
         GeometryReader {
             // For Animation Purpose
@@ -19,7 +22,15 @@ struct Recents: View {
                 ScrollView(.vertical) {
                     LazyVStack(spacing: 10, pinnedViews: [.sectionHeaders]) {
                         Section {
+                            //  Date Filter Button
+                            Button(action: {}, label: {
+                                Text("\(format(date: startDate,format: "dd – MMM yy")) to \(format(date: endDate,format: "dd – MMM yy"))")
+                                    .font(.caption2)
+                                    .foregroundStyle(.gray)
+                            })
+                                .hSpacing(.leading)
                             
+                            // Card View
                         } header: {
                             HeaderView(size)
                         }
@@ -44,6 +55,10 @@ struct Recents: View {
                         .foregroundStyle(.gray)
                 }
             })
+            .visualEffect { content, geometryProxy in
+                content
+                    .scaleEffect(headerScale(size, proxy: geometryProxy), anchor: .topLeading)
+            }
             
             Spacer(minLength: 0)
             
@@ -59,13 +74,36 @@ struct Recents: View {
                     .contentShape(Circle())
             }
         }
-        .padding(.bottom, 5)
+        .padding(.bottom, userName.isEmpty ? 10 : 5)
         .background {
-            Rectangle()
-                .fill(.ultraThinMaterial)
-                .padding(.horizontal, -15)
-                .padding(.top,-(safeArea.top + 15))
-        }
+            VStack(spacing: 0) {
+                Rectangle()
+                    .fill(.ultraThinMaterial)
+                
+                Divider()
+            }
+            .visualEffect { content, geometryProxy in
+                content
+                    .opacity(headerBGOpacity(geometryProxy))
+            }
+            .padding(.horizontal, -15)
+            .padding(.top, -(safeArea.top + 15))
+            }
+    }
+    
+    func headerBGOpacity(_ proxy: GeometryProxy) -> CGFloat {
+        let minY = proxy.frame(in: .scrollView).minY + safeArea.top
+        return minY > 0 ? 0 : (-minY / 15)
+    }
+    
+    func headerScale(_ size: CGSize, proxy: GeometryProxy) -> CGFloat {
+        let minY = proxy.frame(in: .scrollView).minY
+        let screenHeight = size.height
+        
+        let progress = minY / screenHeight
+        let scale = (min(max(progress, 0), 1)) * 0.6
+        
+        return 1 + scale
     }
 }
 
